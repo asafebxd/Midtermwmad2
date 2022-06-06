67de0d56f6ae0cba245d626e046e41b92d93b16f
Atsuya Tanaka <tenmusu007@gmail.com>
2022-06-06 11:07:46 -0700

    fetch

---
+++ js/index.js
@@ -0,0 +1,11 @@
+async function gettingData(){
+    try{
+        const response = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=49.246292&lon=-123.116226&appid=cfcca1b79e2e886dd195b9a2079d6eb1')
+        const myJson = await response.json()
+        console.log(myJson);
+        // data = [...myJson]
+    }catch(error){
+        console.error(error);
+    }
+}
+gettingData()
