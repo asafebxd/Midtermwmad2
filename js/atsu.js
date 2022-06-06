9c18cfc515c62ef9a1cd6ac943b06d8cbf182780
Atsuya Tanaka <tenmusu007@gmail.com>
2022-06-06 11:59:53 -0700

    import data

---
+++ js/atsu.js
@@ -0,0 +1,5 @@
+import { gettingData } from "./index.js";
+// import {data} from "./index"
+
+// console.log(data)
+gettingData()
--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
-async function gettingData(){
+export let data 
+export async function gettingData(){
     try{
         const response = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=49.246292&lon=-123.116226&appid=cfcca1b79e2e886dd195b9a2079d6eb1')
         const myJson = await response.json()
         console.log(myJson);
-        // data = [...myJson]
+        data = {...myJson}
+        // console.log(data)
     }catch(error){
         console.error(error);
     }
 }
-gettingData()
