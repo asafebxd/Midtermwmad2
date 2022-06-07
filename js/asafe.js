719b7de86cac23b92623c1e5a28a05a55e9d1bd5
Asafe <alves.asafecabral@gmail.com>
2022-06-07 11:32:45 -0700

    forCast data

--- js/asafe.js
+++ js/asafe.js
@@ -1,4 +1,27 @@
-import { gettingData } from "./index.js";
-
-
-gettingData()
+import { gettingDataFor } from "./index.js";
+// let test
+gettingDataFor().then((data) => {
+    console.log(data)
+    let test = Array(data)
+    // const forCast = Array(data).map((item,index) => {
+    //     console.log(item.list);
+    //     item.list.forEach(Element => {
+    //         if (Element === item.list[0] || ) {
+    //             console.log(Element)
+    //         }else{
+    //             console.log("no")
+            
+    //     }
+    // });
+    // });
+    // const 5daysCast = []
+    // const 3hoursCast = []
+    console.log(data.list);
+    for (let i = 0; i < data.list.lenght; i++) {
+        console.log(i)
+    }
+});
+console.log(test);
+// for (let i = 0; i < data.list.lenght; i++) {
+//     console.log(i)
+// }
--- js/index.js
+++ js/index.js
@@ -1,11 +1,25 @@
-import { apiKey } from "../apikey.js";
-// console.log(apiKey.weather)
+// import { apiKey } from "./apikey.js";
+// // console.log(apiKey.weather)
 
 
-export async function gettingData(){
+// export async function gettingData(){
+//     try{
+//         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=49.246292&lon=-123.116226&appid=${apiKey.weather}&units=metric`)
+//         console.log(response)
+//         const myJson = await response.json()
+//         // console.log(myJson);
+//         let data = {...myJson};
+//         // console.log(data); 
+//         return data       
+//     }catch(error){
+//         console.error(error);
+//     }
+// }
+
+export async function gettingDataFor(){
     try{
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=49.246292&lon=-123.116226&appid=${apiKey.weather}&units=metric`)
-        console.log(response)
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=35&lon=139&appid=8392b61376612fdef1c2d721674c97b5`)
+        // console.log(response)
         const myJson = await response.json()
         // console.log(myJson);
         let data = {...myJson};
@@ -15,3 +29,4 @@ export async function gettingData(){
         console.error(error);
     }
 }
+
