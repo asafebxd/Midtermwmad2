10951dcb2af05fc2512acb331cd40b6383c340cf
Asafe <alves.asafecabral@gmail.com>
2022-06-10 08:47:39 -0700

    css and forcast data

--- js/asafe.js
+++ js/asafe.js
@@ -1,9 +1,14 @@
 import { gettingDataFor } from "./index.js";
 
-const section = document.getElementsByClassName("container-forcast")[0]
+const sectionDay = document.getElementsByClassName("container-forDay")[0]
 
-const fordiv = document.createElement("div")
-section.append(fordiv)
+const forDay = document.createElement("div")
+sectionDay.append(forDay)
+
+const sectionHour = document.getElementsByClassName("container-forHour")[0]
+
+const forHour = document.createElement("div")
+sectionHour.append(forHour)
 
 gettingDataFor().then((data) => {
     console.log(data)
@@ -19,18 +24,6 @@ gettingDataFor().then((data) => {
 
 //     // return forCast
 // }})
-
-const createHtml = forCast.map((item) => {
-  return (
-    `
-    <div>
-      <p class="test">${item.dt_text}</p>
-    </div>
-    `
-  )
-})
-fordiv.innerHTML = createHtml.join("")
-
     const daysCast = []
     const hoursCast = []   
         
@@ -45,5 +38,51 @@ fordiv.innerHTML = createHtml.join("")
     }
     console.log(hoursCast)
 
+    const createHtml = daysCast.map((item) => {
+      console.log(item);
+      return (
+        `
+        <div class="card-weather">
+          <img src="http://openweathermap.org/img/wn/${
+                  item.weather[0].icon
+                }@2x.png" />
+          <div>
+            <p class="test">${item.dt_txt}</p>
+            <p>${item.weather[0].description}</p>
+            <p>${parseInt(item.main.temp)} ℃</p>
+            <span>${parseInt(item.main.temp_max)} 
+            ℃</span> / <span>${parseInt(item.main.temp_min)} ℃</span>
+          </div>
+        </div>
+        `
+      )
+    })
+    forDay.className = "continer-flex"
+    forDay.innerHTML = createHtml.join("")
+    
+
+        
+    const createhourHTML = hoursCast.map((item) => {
+      console.log(item);
+      return (
+        `
+        <div class="card-weather">
+          <img src="http://openweathermap.org/img/wn/${
+                  item.weather[0].icon
+                }@2x.png" />
+          <div>
+            <p class="test">${item.dt_txt}</p>
+            <p>${item.weather[0].description}</p>
+            <p>${parseInt(item.main.temp)} ℃</p>
+            <span>${parseInt(item.main.temp_max)} 
+            ℃</span> / <span>${parseInt(item.main.temp_min)} ℃</span>
+          </div>
+        </div>
+        `
+      )
+    })
+    forHour.className = "continer-flex"
+    forHour.innerHTML = createhourHTML.join("")
+        
 
 });
